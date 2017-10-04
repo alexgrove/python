6a3820ae1c3a029666c840fc746f6f86bbc56694
alexgrove <alexander_grove@hotmail.com>
2017-10-04 10:49:44 -0600

    Add files via upload

---
+++ quest.py
@@ -0,0 +1,5 @@
+name = input("What is your name?: ")
+quest = input("What is your quest?: ")
+colour = input("What is your favourite colour?: ")
+
+print ("Ah, so your name is %s, your quest is %s, and your favourite colour is %s" % (name, quest, colour))
